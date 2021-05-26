b711b2c75b60bc86f51a7a8a5cc9b4255fa58460
kavinkavin16 <47984191+kavinkavin16@users.noreply.github.com>
2021-05-26 14:37:51 +0530

    Update product.sql

--- product.sql
+++ product.sql
@@ -1,7 +1,7 @@
-drop table product;
-create table product(id number,name varchar(200),expiry_date date);
-insert into product values(1,'Pen',DATE '2023-01-01');
-insert into product values(2,'Pencil',DATE '2023-01-01');
-insert into product values(3,'Milk',DATE '2023-01-01');
+drop table product_2595;
+create table product_2595(id number,name varchar(200),expiry_date date);
+insert into product_2595 values(1,'Pen',DATE '2023-01-01');
+insert into product_2595 values(2,'Pencil',DATE '2023-01-01');
+insert into product_2595 values(3,'Milk',DATE '2023-01-01');
 commit;
-select * from product;
+select * from product_2595;
